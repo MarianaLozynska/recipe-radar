b5122bdc92b09bf97e61ff890cedbe914f4c2cd2
Mariana Lozynska <mariana.lozynska@imgarena.com>
2024-10-04 22:56:35 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl mb-4 text-red-500">Recipe Radar</h1>
+    </div>
+  );
+};
+
+export default App;
